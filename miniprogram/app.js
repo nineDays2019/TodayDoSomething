6d2727679fba65a74470bef353d5b936262f8a10
Juhezi <juhezix@163.com>
2019-02-12 17:53:50 +0800

    [Add] 游客状态

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,15 +1,28 @@
-//app.js
 App({
-  onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        traceUser: true,
-      })
+    onLaunch: function () {
+        wx.login({
+            success: res => {
+                console.log("登录成功")
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            }
+        });
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            this.globalData.userInfo = res.userInfo;
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res);
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    },
+    globalData: {
+        userInfo: null
     }
-
-    this.globalData = {}
-  }
-})
+});
