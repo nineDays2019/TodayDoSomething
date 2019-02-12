0d3f2f24ffbbe57dd641e94446b3cc686e208d7f
Juhezi <juhezix@163.com>
2019-02-13 00:20:35 +0800

    [Add] Local

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,7 +7,8 @@ Page({
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
         description: "一款很简单的清单软件\n今日事今日毕！",  // 从服务器下发，不过意义不大，只有游客才会看到这个页面
-        things: []
+        things: [],
+        value: ""   // 默认值
     },
     onLoad: function () {
         if (app.globalData.userInfo) {
@@ -45,16 +46,48 @@ Page({
             hasUserInfo: true
         });
     },
-    addThing: function () {
+    addThing: function (event) {
         // 暂时为本地方法，所以需要本地生成 id
-        let thing = Thing(new Date().getTime().toString(), "hello world");
-        this.things.push(thing);
-        this.setData(); // 更新页面
+        let thing = new Thing(new Date().getTime().toString(), event.detail.value);
+        let list = this.data.things;
+        if (list) {
+            list.push(thing);
+        } else {
+            list = [thing];
+        }
+        console.log(list);
+        this.setData({
+            things: list,
+            value: ""
+        }); // 更新页面
     },
-    complete: function (index) {
-        this.things[index].complete();
+    complete: function (event) {
+        let idStr = event.currentTarget.id;
+        let id = parseInt(idStr);
+        let list = this.data.things;
+        list[id].complete();
+        this.setData({
+            things: list
+        });
     },
-    getThings: function() {
+    getThings: function () {
 
     }
 });
+
+class Thing {
+
+    constructor(id, content) {
+        this.id = id;
+        this.content = content;
+        this.isComplete = false;  // 是否完成，初始状态为 falae
+        this.createTime = new Date().getTime();
+        this.completeTime = null;   // 完成时间
+    }
+
+    complete() {
+        this.isComplete = true;
+        this.completeTime = new Date().getTime();
+    }
+
+}
